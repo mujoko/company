0b967e87ab93f8ad4700dc3ea501f171a2acd61c
Mujoko <mujoko.mujoko@gmail.com>
2025-03-23 13:52:11 +0700

    Add Template hardcode

--- djanggo_project/urls.py
+++ djanggo_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # new
+
+from django.contrib import admin
+from django.urls import path, include  # new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("pages.urls")),  # new
 ]
